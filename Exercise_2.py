f790e7861431302fc8be1cef77d1651c31681b70
Nakul Patel <nakulpatel86@yahoo.com>
2024-05-12 22:04:21 -0700

    Update Exercise_2.py
    Added solution for Stack using lined list in python

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,9 @@
+# Time Complexity:
+#O(1) for push(), pop() 
+
+# Space complexity
+# O(1) for isEmpty(), pop()
+# O(n) for push()
 
 class Node:
     def __init__(self, data):
@@ -6,12 +12,29 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.firstNode = None
         
     def push(self, data):
-        
+        if self.firstNode == None:
+            self.firstNode = Node(data)
+        else:
+            newNode = Node(data)
+            newNode.next = self.firstNode  #point the next pointer of above created Node to prevous Node in stack
+            self.firstNode  = newNode     # update the head(pointer) to latest Node
+
     def pop(self):
         
+        # check if the stack is empty() , if empty, return None
+        if self.firstNode == None:
+            return None 
+        else:
+            popped = self.firstNode
+            self.firstNode = popped.next
+            popped.next = None
+            return popped.data
+
 a_stack = Stack()
+
 while True:
     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
     print('push <value>')
